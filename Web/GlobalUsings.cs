46c6e581365e9ae8fe1d6e50f46a78e5c99605b8
Shveder <shvedegor7687@gmail.com>
2024-12-06 02:21:25 +0300

    Edit: master - edit GlobalUsings.cs

--- Web/GlobalUsings.cs
+++ Web/GlobalUsings.cs
@@ -16,3 +16,6 @@ global using Microsoft.Extensions.FileProviders;
 global using Web.Middlewares;
 global using Common;
 global using Application.Services.Base;
+global using System.Text;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.IdentityModel.Tokens;
